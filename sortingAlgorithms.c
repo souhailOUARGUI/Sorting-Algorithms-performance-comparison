65f68c181e71a194dcfd4114ff91c85dd1d032b9
souhailOUARGUI <80362551+souhailOUARGUI@users.noreply.github.com>
2023-10-25 11:36:10 +0100

    starting point

---
+++ sortingAlgorithms.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include <string.h>
+
+///     machine time calculating function ///
+double time_elapsed(clock_t start, clock_t end)
+{
+    return 0;
+}
+
+///   Sorting Algorithms   ///
+void bubble(int T[], int n)
+{
+}
+void insertion(int T[], int n)
+{
+}
+void selection(int T[], int n)
+{
+}
+void shell(int T[], int n)
+{
+}
+void merge(int T[], int n)
+{
+}
+
+int main()
+{
+
+    return 0;
+}
